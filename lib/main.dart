6294958f0edc1fe50bb01eca1ca0c2937a979e40
suhrobcoder <suhrobcoder@gmail.com>
2021-07-31 15:58:40 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tasky',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Scaffold(
+        body: Center(child: Text("Hello World")),
+      ),
+    );
+  }
+}
