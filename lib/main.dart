bcbfa23b3cfe51b048d1f6f6714dcddf3f19c3d2
suhrobcoder <suhrobcoder@gmail.com>
2021-08-02 21:16:32 +0500

    service locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:tasky/auth/presentation/pages/splash_page.dart';
+import 'package:tasky/service_locator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await setup();
   runApp(const MyApp());
 }
 
